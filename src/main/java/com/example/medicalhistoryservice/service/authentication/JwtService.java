84ad898becdc9926a07a2d15f947d7f2f16cdbd1
HasilbekR <hreymbaev@gmail.com>
2023-08-16 22:53:49 +0500

    jwt filter and security configurations are added

---
+++ src/main/java/com/example/medicalhistoryservice/service/authentication/JwtService.java
@@ -0,0 +1,18 @@
+package com.example.medicalhistoryservice.service.authentication;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    public Jws<Claims> extractToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+    }
+}
